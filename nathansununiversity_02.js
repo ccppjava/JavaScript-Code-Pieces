554ec2d76adcfb283d2c1e4489da5b7f09169ed3
yuqi <coolcnid@gmail.com>
2012-04-25 23:16:23 +0100

    add the lesson two page 5 task, work in progress

---
+++ nathansununiversity_02.js
@@ -0,0 +1,49 @@
+// http://nathansuniversity.com/music.html
+// this file supposed to work with node.js 
+//
+// following compile function is from last lesson
+var compile = function (expr) {
+    
+    var result = [];
+
+    (function endTime(time, expr, ref) {
+        // your code here
+        (function(expr) {
+            if (expr.tag === 'note') {
+                expr.start = ref;
+                result.push(expr);
+                
+                time += expr.dur;
+            } else if (expr.tag === 'par') {
+                var endLeft = endTime(0, expr.left, time);
+                var endRight = endTime(0, expr.right, time);
+                time += endLeft > endRight ? endLeft : endRight;
+            } else {
+                arguments.callee(expr.left);
+                arguments.callee(expr.right);
+            }
+        })(expr);
+        
+        return time;
+    })(0, expr, 0);
+        
+    return result;
+};
+
+// testing code
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+            { tag: 'seq',
+              left: { tag: 'note', pitch: 'a4', dur: 250 },
+              right: { tag: 'note', pitch: 'b4', dur: 250 } 
+            },
+      right:
+            { tag: 'seq',
+              left: { tag: 'note', pitch: 'c4', dur: 500 },
+              right: { tag: 'note', pitch: 'd4', dur: 500 } 
+            } 
+    };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
