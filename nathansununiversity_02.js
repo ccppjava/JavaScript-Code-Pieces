c7151078cc51594e2ba003d7bd7f47f6e81354c3
yuqi <coolcnid@gmail.com>
2012-04-26 11:29:27 +0100

    add the suppport for repeat section

--- nathansununiversity_02.js
+++ nathansununiversity_02.js
@@ -9,6 +9,24 @@ var convertPitch = function(pitch) {
     }
 };
 
+// my compile/edit in place cause issue when repeat is added
+// e.g. output: { tag: 'note', pitch: undefined, dur: 220, start: 940 },
+// thus need following shallow copy function, using jQuery style
+var copyObject = function() {
+    var args = Array.prototype.slice.apply(arguments);
+    //console.log(args);
+    var target = args.shift(0) || {};
+    for (var i = 0; i < args.length; ++i) {
+        for (var key in args[i]) {
+            if (args[i].hasOwnProperty(key)) {
+                target[key] = args[i][key];
+            }
+        }
+    }
+
+    return target;
+};
+
 // following compile function is from last lesson
 var compile = function (expr) {
     
@@ -18,17 +36,29 @@ var compile = function (expr) {
         // your code here
         (function(expr) {
             if (expr.tag === 'note' || expr.tag === 'rest') {
+                /*
                 expr.start = time || ref;
                 if (expr.pitch) {
                     expr.pitch = convertPitch(expr.pitch);
                 }
                 result.push(expr);
+                */
+                var note = copyObject({}, expr, {start: time || ref});
+                if (expr.pitch) {
+                    note = copyObject(note, {pitch: convertPitch(expr.pitch)});
+                }
+
+                result.push(note);
                 
                 time += expr.dur;
             } else if (expr.tag === 'par') {
                 var endLeft = endTime(0, expr.left, time);
                 var endRight = endTime(0, expr.right, time);
                 time += endLeft > endRight ? endLeft : endRight;
+            } else if (expr.tag === 'repeat') {
+                for (var i = 0; i < expr.count; ++i) {
+                    arguments.callee(expr.section);
+                }
             } else {
                 arguments.callee(expr.left);
                 arguments.callee(expr.right);
@@ -54,10 +84,19 @@ var melody_mus =
             },
       right:
             { tag: 'seq',
-              left: { tag: 'note', pitch: 'c4', dur: 500 },
+              left: { tag: 'repeat', 
+                      section: { tag: 'note', pitch: 'c4', dur: 220 }, 
+                      count: 3 
+                    },
               right: { tag: 'note', pitch: 'd4', dur: 500 } 
             } 
     };
 
 console.log(melody_mus);
 console.log(compile(melody_mus));
+
+// testing the copyObject function
+//console.log(copyObject());
+//console.log(copyObject({}));
+//console.log(copyObject({}, {a: 1}));
+//console.log(copyObject({}, {a: 1, b: 2}, {b: 3}));
